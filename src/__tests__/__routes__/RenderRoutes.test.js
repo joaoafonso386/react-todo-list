e5c46aca2cffbbf06a2d5596db811c6abe1e1380
João Afonso <joaoafonso386@gmail.com>
2022-09-17 14:47:31 +0100

    Added tests for application routing

---
+++ src/__tests__/__routes__/RenderRoutes.test.js
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import RenderRoutes from "../../routes/RenderRoutes/RenderRoutes";
+import Layout from "../../views/Layout/Layout";
+
+test("Test if app renders routes correctly and changes page on user click", async () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <Layout>
+        <RenderRoutes />
+      </Layout>
+    </Router>
+  );
+
+  const user = userEvent.setup();
+  expect(screen.getByText("Intro")).toBeInTheDocument();
+
+  await user.click(screen.getByText("Todo List"));
+
+  expect(await screen.findByText("Task")).toBeInTheDocument();
+});
